b0e94b3589618caadd4144caa3bccf6cfaeecd34
TSK <rdv260991@gmail.com>
2017-12-09 13:46:59 +0100

    Fixed bug where stats would not be calculated correctly after evolution

--- engine/evos_moves.asm
+++ engine/evos_moves.asm
@@ -177,17 +177,7 @@ Evolution_PartyMonLoop: ; loop over party mons
 	ld a, [wd11e]
 	push af
 	ld a, [wd0b5]
-	ld [wd11e], a
-	predef IndexToPokedex
-	ld a, [wd11e]
-	dec a
-	ld hl, BaseStats
-	ld bc, MonBaseStatsEnd - MonBaseStats
-	call AddNTimes
-	ld de, wMonHeader
-	call CopyData
-	ld a, [wd0b5]
-	ld [wMonHIndex], a
+	call GetMonHeader
 	pop af
 	ld [wd11e], a
 	ld hl, wLoadedMonHPExp - 1
